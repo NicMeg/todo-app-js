b5ae8e356e59aed16780ee1d83015626be75543f
Nic Meg <nicmeg54@gmail.com>
2020-06-16 19:52:26 +1000

    Code Formatting Updated

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-const button = document.querySelector('button');
-const items = document.querySelectorAll('li');
-const ul = document.querySelector('ul');
+const button = document.querySelector("button");
+const items = document.querySelectorAll("li");
+const ul = document.querySelector("ul");
 
-button.addEventListener('click', () => {
-  if (confirm('Are you SURE you want to delete your list?')) {
-    ul.remove()
+button.addEventListener("click", () => {
+  if (confirm("Are you SURE you want to delete your list?")) {
+    ul.remove();
     // Save it!
   } else {
     // Do nothing!
   }
-
 });
 
-
-items.forEach(item => {
-item.addEventListener('click', e => {
-  console.log(e.target);
-  console.log(item);
-  e.target.style.textDecoration = 'line-through';
+items.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target);
+    console.log(item);
+    e.target.style.textDecoration = "line-through";
+  });
 });
-})
 
 // items.forEach(item => {
 //   item.addEventListener('click', e => {
