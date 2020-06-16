5430043cc941f54e4670affc96bd1238fbd41127
Nic Meg <nicmeg54@gmail.com>
2020-06-16 20:29:03 +1000

    Update script.js
    Add button created

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+const addTask = document.querySelector(".add");
 const button = document.querySelector("button");
 const items = document.querySelectorAll("li");
 const ul = document.querySelector("ul");
 
+// Adding new items
+// console.log("addTask");
+addTask.addEventListener('click', () => {
+  ul.innerHTML += "<li> Something New </li>";
+});
+document.querySelector(".content");
+
+// Delete Item Once Clicked
+items.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target);
+    console.log(item);
+    //e.target.style.textDecoration = "line-through";
+    e.target.remove();
+  });
+});
+
+// Delete All To-Do Items
 button.addEventListener("click", () => {
   if (confirm("Are you SURE you want to delete your list?")) {
     ul.remove();
@@ -11,17 +30,8 @@ button.addEventListener("click", () => {
   }
 });
 
-items.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    console.log(e.target);
-    console.log(item);
-    e.target.style.textDecoration = "line-through";
-  });
-});
-
 // items.forEach(item => {
 //   item.addEventListener('click', e => {
 //     ul.remove()
-
 //   });
 // })
